c40b325dcd06aa9125c33360418455529bc99bd3
Danial <143175394+Danial-Doskuzhinov@users.noreply.github.com>
2024-07-13 13:41:29 +0500

    Update vite.config.ts
    	test: {
    		include: ['src/**/*.{test,spec}.{js,ts}']
    	}

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,5 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	plugins: [sveltekit()],
 	base:"/Multi-Step-Form/",
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+
 });
